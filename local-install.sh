6b8eecfbb89d6835d511211860d8af0ea06f6d70
Joe <54695266+jdboris@users.noreply.github.com>
2022-01-15 13:52:15 -0500

    Create local-install.sh for installing local packages without bugs

---
+++ local-install.sh
@@ -0,0 +1,14 @@
+originalDir=$(pwd)
+repoDir=C:/Users/Joe/Source/Repos
+
+for packagePath in "$@"
+do
+    cd $packagePath
+    packageName=$(cut -d "=" -f 2 <<< $(npm run env | grep "npm_package_name"))
+    npm run build
+    mkdir --parents $repoDir/@local/$packageName
+    mv $repoDir/@local/$(npm pack --pack-destination=$repoDir/@local) $repoDir/@local/$packageName.tgz
+    cd $originalDir
+    npm remove $packageName
+    npm i --save-dev $repoDir/@local/$packageName.tgz
+done
