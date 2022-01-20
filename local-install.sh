115daeb9e0e0eafdf99e12514245f98578ea0214
Joe <54695266+jdboris@users.noreply.github.com>
2022-01-20 10:34:27 -0500

    Update version number to get latest dependencies

--- local-install.sh
+++ local-install.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
 originalDir=$(pwd)
 repoDir=C:/Users/Joe/Source/Repos
 
 for packagePath in "$@"
 do
-    cd $packagePath
-    packageName=$(cut -d "=" -f 2 <<< $(npm run env | grep "npm_package_name"))
-    npm run build
+    cd $packagePath || { exec bash; }
+    packageName=$(cut -d "=" -f 2 <<< $(npm run env | grep "npm_package_name")) || { exec bash; }
+    npm run build || { exec bash; }
     mkdir --parents $repoDir/@local/$packageName
     mv $repoDir/@local/$(npm pack --pack-destination=$repoDir/@local) $repoDir/@local/$packageName.tgz
-    cd $originalDir
+    cd $originalDir || { exec bash; }
     # npm remove $packageName
     npm i --save-dev -f $repoDir/@local/$packageName.tgz
 done
