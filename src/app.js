58889f94a70a69e299ec0febcc755f3aa90aa256
Joe <54695266+jdboris@users.noreply.github.com>
2022-02-26 00:04:13 -0500

    Add footerLinks

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import { StreamSiteApp } from "@jdboris/stream-site";
+import DiscordIcon from "./discord-icon.svg";
+import SteamIcon from "./steam-icon.svg";
 
 function App() {
-  return <StreamSiteApp />;
+  return (
+    <StreamSiteApp
+      footerLinks={[
+        <a href="https://discord.gg/Kn57rKMm" target="_blank" rel="noreferrer">
+          <img src={DiscordIcon} />
+        </a>,
+        <a
+          href="https://steamcommunity.com/groups/xxxxxxxx-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={SteamIcon} />
+        </a>,
+      ]}
+    />
+  );
 }
 
 export default App;
