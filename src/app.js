da10e81c1f891e17e6e234cb3ea57d9902e3ac17
Joe <54695266+jdboris@users.noreply.github.com>
2022-02-28 10:53:52 -0500

    Update to permanent discord link

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <StreamSiteApp
       footerLinks={[
-        <a href="https://discord.gg/Kn57rKMm" target="_blank" rel="noreferrer">
+        <a href="https://discord.gg/GXWMdPd" target="_blank" rel="noreferrer">
           <img src={DiscordIcon} />
         </a>,
         <a
