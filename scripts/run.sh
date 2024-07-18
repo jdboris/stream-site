70c4eea3fcad60580ea85d187d88c3fa92095a62
Joe <54695266+jdboris@users.noreply.github.com>
2024-07-18 03:45:01 -0400

    Fix phpmyadmin port

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 docker compose down
 docker compose pull
-docker compose --progress=plain -p stream-site build --no-cache && docker compose --progress=plain -p stream-site up --watch
+# docker compose --progress=plain -p stream-site build --no-cache && docker compose --progress=plain -p stream-site up --watch
+docker compose --progress=plain -p stream-site build && docker compose --progress=plain -p stream-site up --watch
 
 $SHELL
