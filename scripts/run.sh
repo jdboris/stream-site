f902a38362e8409f474eee64809ba392fbbbc700
Joe <54695266+jdboris@users.noreply.github.com>
2024-07-18 09:00:12 -0400

    Add docker prune to run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,6 @@ docker compose down
 docker compose pull
 # docker compose --progress=plain -p stream-site build --no-cache && docker compose --progress=plain -p stream-site up --watch
 docker compose --progress=plain -p stream-site build && docker compose --progress=plain -p stream-site up --watch
+docker system prune --all --force
 
 $SHELL
