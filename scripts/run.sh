5b639cc7a1933a61c4c5d84cd5368398bd27b9d0
Joe <54695266+jdboris@users.noreply.github.com>
2024-10-14 03:17:22 -0400

    Switch to prod as the default compose file and override with dev, add profiles and build stages for more control

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,12 @@
 docker compose down
 docker compose pull
-# docker compose --progress=plain -p stream-site build --no-cache && docker compose --progress=plain -p stream-site up --watch
-docker compose --progress=plain -p stream-site build && docker compose --progress=plain -p stream-site up --watch
-# docker compose --progress=plain -p stream-site up
 
+# docker compose --progress=plain -p stream-site build --no-cache && docker compose --progress=plain -p stream-site -f docker-compose.yml -f docker-compose.dev.yml up --watch
+
+docker compose --progress=plain -p stream-site --profile development -f docker-compose.yml -f docker-compose.dev.yml build &&
+  docker compose --progress=plain -p stream-site --profile development -f docker-compose.yml -f docker-compose.dev.yml up --watch
+
+# docker compose --progress=plain -p stream-site -f docker-compose.yml -f docker-compose.dev.yml up
 # docker system prune --all --force
 
 $SHELL
