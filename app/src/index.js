2f4c25edde957a7ba58f5b311adb1ac0fdc10104
Joe <54695266+jdboris@users.noreply.github.com>
2024-10-15 02:35:53 -0400

    Refactor express app to only start acme challenge servers if cert files are missing, and to close them when they appear

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,8 @@
-import { createServer as createServerHttps } from "https";
-import { createServer as createServerHttp } from "http";
+import {
+  createServer as createServerHttps,
+  Server as HttpsServer,
+} from "https";
+import { createServer as createServerHttp, Server as HttpServer } from "http";
 import express from "express";
 import session from "express-session";
 import { fileURLToPath } from "url";
@@ -19,93 +22,104 @@ import requestLogger from "./utils/request-logger.js";
 import { existsSync, readFileSync } from "fs";
 import HttpError from "./utils/http-error.js";
 
-const { PORT, PUBLIC_DOMAIN } = process.env;
+const { NODE_ENV, PORT, PUBLIC_DOMAIN } = process.env;
 
-const app = express();
+/** @type {HttpsServer|null} */
+let server = null;
+/** @type {HttpServer|null} */
+let httpAcmeChallengeServer = null;
+/** @type {HttpsServer|null} */
+let httpsAcmeChallengeServer = null;
 
-if (process.env.NODE_ENV != "production") {
-  // NOTE: Must come BEFORE express.json() to get raw text body
-  app.use(requestLogger);
-}
-
-// Certbot ACME challenge (https option for use with proxies like Cloudflare)...
-app.use(
-  "/.well-known",
-  express.static(path.join(__dirname, "../client/.well-known"))
-);
-
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.use(express.json());
-
-app.use(
-  session({
-    secret: await readFileSync(
-      path.resolve(__dirname, "../secrets/session-key.key"),
-      "utf8"
-    ),
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: true },
-  })
-);
-
-// Routers
-app.use(authRouter);
-app.use(bannersRouter);
-app.use(channelsRouter);
-app.use(settingsRouter);
-app.use(streamEventsRouter);
-app.use(suggestionsRouter);
-
-app.use(
-  /**
-   * @param {Error} error
-   * @param {import("express").Request} req
-   * @param {import("express").Response} res
-   * @param {import("express").NextFunction} next
-   *
-   **/
-  async (error, req, res, next) => {
-    if (error instanceof HttpError) {
-      res.status(error.status).send({
-        error: error.message,
-      });
-      return;
-    }
-
-    console.error(error);
-    res.status(500).send({
-      error: "Something went wrong. Please try again.",
-    });
-  }
-);
-
-const keyPath = path.resolve(
-  __dirname,
-  `../../certbot/volumes/etc/letsencrypt/live/${PUBLIC_DOMAIN}/privkey.pem`
-);
-
-const certPath = path.resolve(
-  __dirname,
-  `../../certbot/volumes/etc/letsencrypt/live/${PUBLIC_DOMAIN}/fullchain.pem`
-);
-
+// Periodically check for cert files and start acme challenge servers if they're missing.
 const interval = setInterval(async () => {
-  if (startServer()) {
+  const keyPath = path.resolve(
+    __dirname,
+    `../../certbot/volumes/etc/letsencrypt/live/${PUBLIC_DOMAIN}/privkey.pem`
+  );
+
+  const certPath = path.resolve(
+    __dirname,
+    `../../certbot/volumes/etc/letsencrypt/live/${PUBLIC_DOMAIN}/fullchain.pem`
+  );
+
+  if (existsSync(keyPath) && existsSync(certPath)) {
     clearInterval(interval);
+
+    if (httpAcmeChallengeServer) httpAcmeChallengeServer.close();
+    if (httpsAcmeChallengeServer) httpsAcmeChallengeServer.close();
+    server = startServer(keyPath, certPath);
+    return;
   }
+
+  if (server) server.close();
+  httpAcmeChallengeServer = startHttpAcmeChallengeServer();
+  httpsAcmeChallengeServer = startHttpsAcmeChallengeServer();
 }, 5000);
 
-function startServer() {
-  if (!existsSync(keyPath) || !existsSync(certPath)) {
-    return false;
+/**
+ * @param {string} certPath
+ * @param {string} keyPath
+ * */
+function startServer(keyPath, certPath) {
+  const app = express();
+
+  if (NODE_ENV != "production") {
+    // NOTE: Must come BEFORE express.json() to get raw text body
+    app.use(requestLogger);
   }
+
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.use(express.json());
+
+  app.use(
+    session({
+      secret: readFileSync(
+        path.resolve(__dirname, "../secrets/session-key.key"),
+        "utf8"
+      ),
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: true },
+    })
+  );
+
+  // Routers
+  app.use(authRouter);
+  app.use(bannersRouter);
+  app.use(channelsRouter);
+  app.use(settingsRouter);
+  app.use(streamEventsRouter);
+  app.use(suggestionsRouter);
+
+  app.use(
+    /**
+     * @param {Error} error
+     * @param {import("express").Request} req
+     * @param {import("express").Response} res
+     * @param {import("express").NextFunction} next
+     *
+     **/
+    async (error, req, res, next) => {
+      if (error instanceof HttpError) {
+        res.status(error.status).send({
+          error: error.message,
+        });
+        return;
+      }
+
+      console.error(error);
+      res.status(500).send({
+        error: "Something went wrong. Please try again.",
+      });
+    }
+  );
 
   const privateKey = readFileSync(keyPath, "utf8");
   const certificate = readFileSync(certPath, "utf8");
 
-  createServerHttps(
+  return createServerHttps(
     {
       key: privateKey,
       cert: certificate,
@@ -114,26 +128,57 @@ function startServer() {
   ).listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
   });
-
-  return true;
 }
 
-const httpApp = express();
+// HTTPS (in case of proxies like cloudflare)
+function startHttpsAcmeChallengeServer() {
+  // Use self-signed cert for now
+  const keyPath = path.resolve(
+    __dirname,
+    `../../certbot/volumes/etc/letsencrypt/live/localhost/privkey.pem`
+  );
 
-httpApp.get("/healthcheck", (req, res) => {
-  res.send();
-});
+  const certPath = path.resolve(
+    __dirname,
+    `../../certbot/volumes/etc/letsencrypt/live/localhost/fullchain.pem`
+  );
 
-// Certbot ACME challenge...
-httpApp.use(
-  "/.well-known",
-  express.static(path.join(__dirname, "../client/.well-known"))
-);
+  const app = express();
 
-httpApp.use((req, res) => {
-  res.redirect(`https://${PUBLIC_DOMAIN}${req.url}`);
-});
+  app.use(
+    "/.well-known",
+    express.static(path.join(__dirname, "../client/.well-known"))
+  );
 
-createServerHttp(httpApp).listen(80, () => {
-  console.log(`Example app listening on port 80`);
-});
+  const privateKey = readFileSync(keyPath, "utf8");
+  const certificate = readFileSync(certPath, "utf8");
+
+  return createServerHttps(
+    {
+      key: privateKey,
+      cert: certificate,
+    },
+    app
+  ).listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+}
+
+// HTTP
+function startHttpAcmeChallengeServer() {
+  const app = express();
+
+  app.get("/healthcheck", (req, res) => {
+    res.send();
+  });
+
+  // Certbot ACME challenge...
+  app.use(
+    "/.well-known",
+    express.static(path.join(__dirname, "../client/.well-known"))
+  );
+
+  return createServerHttp(app).listen(80, () => {
+    console.log(`Example app listening on port 80`);
+  });
+}
